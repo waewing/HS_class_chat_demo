6286a29a8d676c604f9151adea2eb44298164278
waewing <waewing24@gmail.com>
2026-02-23 10:20:58 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
    <main className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex flex-col items-center p-8">
      <div className="w-full max-w-4xl">
      <h1 className="mb-8 text-6xl font-bold text-center">
-          🏂🏻 🥷🏼 🧑🏾‍✈️ 🏥 <span className="bg-gradient-to-r from-red-600 to-cyan-600
-          bg-clip-text text-transparent">AIs chatbot</span>
+          <span className="bg-gradient-to-r from-red-600 to-cyan-600
+          bg-clip-text text-transparent">Capstone Project AI Chatbot</span>
       </h1>
        <div className="bg-white rounded-2xl shadow-xl p-6">
          <Chatbot/>
